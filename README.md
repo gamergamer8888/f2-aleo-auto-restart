dd0e713eab4df2f77daf015a4a513389d72a7382
gkendiy <1565137256@qq.com>
2024-07-10 17:42:57 +0800

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# f2-aleo-auto-restart
+# f2-aleo-autorestart
+
+#### 注意，本程序需要魔法，因为api地址在墙外，使用clash的tun模式即可
+1. https://www.f2pool.com/user/account#tab-api 
+    - 点击 Request a New Token
+    - 进去后，勾上 hash_rate/worker/list权限，点击提交，复制保存 TOKEN
+2. aleo-autorestart.sh 放入aleo.sh所在目录
+3. 修改aleo-autorestart.sh
+    - URL：默认不用改，除非api用转发
+    - API_TOKEN：前面生成的TOKEN
+    - ACCOUNTNAME：aleo.sh 使用的参数，账户名
+    - WORKERNAME：aleo.sh 使用的参数，矿工名
+4. 进入脚本所在目录， ```sudo chmod +x aleo-autorestart.sh ```，给执行权限
+5. crontab -e 末尾追加 ```*/5 * * * * cd /root/f2-aleo && bash ./aleo-autorestart.sh```
+    - 把 /root/f2-aleo 替换成自己的目录
+    - 每5分钟检查一次，自行修改间隔
+6. 查看日志：tail -f aleo-miner.log
+
---
+++ aleo-autorestart.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+# 适用官方脚本 aleo.sh
+# token获取地址 https://www.f2pool.com/user/account#tab-api
+API_TOKEN=
+# 账户名
+ACCOUNTNAME=
+# 矿工名
+WORKERNAME=
+# api文档地址 https://www.f2pool.com/api_doc
+# 查询矿工列表
+URL=https://api.f2pool.com/v2/hash_rate/worker/list
+# 启动脚本
+CMD="./aleo.sh stratum+ssl://nlb-7b20ejgco2wxsf4av2.cn-chengdu.nlb.aliyuncs.com:4420 $ACCOUNTNAME.$WORKERNAME"
+# 工作目录
+WORKSPACE="$(cd $(dirname $0) && pwd)"
+# 日志文件路径
+LOG_PATH="$WORKSPACE/aleo-miner.log"
+
+# echo $WORKSPACE
+# 安装缺失模块
+if ! which jq > /dev/null; then
+    sudo apt install jq -y
+fi
+# 获取离线矿工名，写入workers数组
+# 测试网 aleo-staging
+# status: 0 在线 1 离线 2 失效
+readarray -t workers < <(curl -X POST $URL --silent \
+     -H 'Content-Type: application/json' \
+     -H "F2P-API-SECRET: $API_TOKEN" \
+     -d '{"mining_user_name":"'"$ACCOUNTNAME"'","currency": "aleo-staging"}' \
+     | jq '[.workers|map(select(.status|.==1)).[].hash_rate_info.name]')
+# 判断是否离线
+if [[ "${workers[@]}"  =~ "$WORKERNAME" ]]; then
+    echo "$WORKERNAME is offline，restart miner..." >> $LOG_PATH
+    # 重新启动miner
+    $CMD
+fi
